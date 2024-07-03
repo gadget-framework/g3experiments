e8da5f9eabc11b80490f93843315fd398c9ea59d
Bjarki Elvarsson <bjarki.elvarsson@hafogvatn.is>
2024-07-03 07:33:10 +0000

    update default lower bounds

--- R/g3a_ss3_bev_holt.R
+++ R/g3a_ss3_bev_holt.R
@@ -1,5 +1,5 @@
 g3a_spawn_recruitment_bevertonholt_ss3 <- function (
-    h = g3_parameterized('srr_h', by_stock = by_stock, lower = 0, upper = 1, value = 0.5),
+    h = g3_parameterized('srr_h', by_stock = by_stock, lower = 0.1, upper = 1, value = 0.5),
     R0 = g3_parameterized('R0', by_stock = by_stock),
     B0 = g3_parameterized('B0', by_stock = by_stock),
     by_stock = TRUE) {
